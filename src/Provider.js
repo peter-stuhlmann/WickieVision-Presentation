94d1cf63b29dbf0c9002883c7b0a5d50f838d68f
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-10-24 16:08:56 +0200

    Use GlobalStyles in ThemeProvider

--- src/Provider.js
+++ src/Provider.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ThemeProvider from 'mdx-deck/dist/Provider';
+import { GlobalStyles } from './components/styled-components/GlobalStyles';
 import { Copyright } from './components/styled-components/Footer';
 
 const Provider = ({ children, ...rest }) => (
   <ThemeProvider {...rest}>
     {children}
+    <GlobalStyles />
     <Copyright url="https://wickievision.de" link="Wickie Vision" />
   </ThemeProvider>
 );
